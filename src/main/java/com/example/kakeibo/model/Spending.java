b164d29f1aa3d4e48f290b92b6d33b6a371241ea
DESKTOP-DCNVLMS\Owner <Korin.Yuta@technopro.com>
2023-05-09 13:30:09 +0900

    Spendingクラス作成

--- src/main/java/com/example/kakeibo/model/Spending.java
+++ src/main/java/com/example/kakeibo/model/Spending.java
@@ -1,7 +1,41 @@
 package com.example.kakeibo.model;
 
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+//支出クラス
+@Entity
+@Data
 public class Spending {
 
+	//Spendingクラスの主キー
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 	
+	//収支金額
+	@NotBlank(message="適切な金額を入力してください。")
+	@Column(length=18)
+	private Integer spendingAmount;
+	
+	//収支の日付
+	LocalDate datetime;
+	
+	//メモ内容
+	@NotBlank(message="適切な内容を入力してください。")
+	@Column(length=18)
+	String memo;
+	
+	//外部キーuser
+	@ManyToOne
+	 User user_id;
 }
